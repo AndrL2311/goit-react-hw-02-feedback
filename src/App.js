f2e8be8f0cf800ab50a1a1bc672b15f57e518a8e
Andrii Lypovetskyi <li_andr@ukr.net>
2021-09-28 22:05:15 +0300

    Добавил разметку

--- src/App.js
+++ src/App.js
@@ -1,25 +1,9 @@
-import Profile from './components/profile/Profile';
-import user from './components/profile/user.json';
-import Statistics from './components/statistics/Statistics';
-import statisticalData from './components/statistics/statistical-data.json';
-import FriendList from './components/friendList/FriendList';
-import friendsData from './components/friendList/friends.json';
-import trasactionData from './components/transactions/transactions.json';
-import TransactionHistory from './components/transactions/TransactionHistory';
+import Feedback from './components/Feedback/Feedback';
 
 function App() {
   return (
     <div>
-      <Profile
-        name={user.name}
-        tag={user.tag}
-        location={user.location}
-        avatar={user.avatar}
-        stats={user.stats}
-      />
-      <Statistics title="Upload stats" stats={statisticalData} />
-      <FriendList friends={friendsData} />
-      <TransactionHistory items={trasactionData} />
+      <Feedback />
     </div>
   );
 }
--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,7 +1,51 @@
-// import s from './';
+import s from './Feedback.module.css';
 
 function Feedback() {
-  return <div>Hi!!!</div>;
+  return (
+    <div className={s.container}>
+      <p className="title">Please leave feedback</p>
+      <ul className={s.controls}>
+        <li>
+          <button className={s.button} type="button">
+            Good
+          </button>
+        </li>
+        <li>
+          <button className={s.button} type="button">
+            Neutral
+          </button>
+        </li>
+        <li>
+          <button className={s.button} type="button">
+            Bad
+          </button>
+        </li>
+      </ul>
+      <ul className="statistics">
+        <p clasName="title">Statistics</p>
+        <li className="item">
+          <span className={s.label}>Good:</span>
+          <span className={s.value}>0</span>
+        </li>
+        <li className="item">
+          <span className={s.label}>Neutral:</span>
+          <span className={s.value}>0</span>
+        </li>
+        <li className="item">
+          <span className={s.label}>Bad:</span>
+          <span className={s.value}>0</span>
+        </li>
+        <li className="item">
+          <span className={s.label}>Total:</span>
+          <span className={s.value}>0</span>
+        </li>
+        <li className="item">
+          <span className={s.label}>Positive Feedback:</span>
+          <span className={s.value}>0%</span>
+        </li>
+      </ul>
+    </div>
+  );
 }
 
 export default Feedback;
